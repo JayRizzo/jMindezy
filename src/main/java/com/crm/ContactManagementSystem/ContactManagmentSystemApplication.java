e900e723ddc6e7567ae843ffdb6945557433a3b4
Jeromie Kirchoff <jahomie04@gmail.com>
2022-07-20 21:00:13 -0500

    Initial Commit

---
+++ src/main/java/com/crm/ContactManagementSystem/ContactManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.crm.ContactManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactManagmentSystemApplication.class, args);
+	}
+
+}
